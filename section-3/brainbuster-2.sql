8ae2b093cf128e455de0c72b5450b81c1f41caf1
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2020-11-27 12:28:15 -0500

    - added comments to brainbuster 2

--- section-3/brainbuster-2.sql
+++ section-3/brainbuster-2.sql
@@ -1,8 +1,10 @@
+#Normal Question Answer
 select count(title), rating
 from film
 group by rating
 ;
 
+#Advanced Question Answer
 select count(title), rating, rental_rate
 from film
 group by rating, rental_rate
