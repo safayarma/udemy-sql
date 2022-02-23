092b5b96715e9dabee9e2d33f7ce6ca540c51570
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-02-22 20:51:28 -0500

    - Revisiting topic/Leaning how to use DISTINCT

--- section-3/brainbuster-7.sql
+++ section-3/brainbuster-7.sql
@@ -1,10 +1,12 @@
-#Number of distinct films rented each month
-
-select
-
-from
-
-where
-
-group by
+#Number of distinct films per month
+SELECT
+	left(r.rental_date, 7) as "Month", count(r.rental_id) as "Total Rentals",
+	count(distinct i.film_id) as "Unique Films Rented", count(r.rental_id)/
+	count(distinct i.film_id) as "Rentals Per Film"
+FROM
+	rental r, inventory i
+WHERE
+	r.inventory_id = i.inventory_id
+GROUP BY
+	1
 ;
--- section-3/distinct.sql
+++ section-3/distinct.sql
@@ -1,12 +1,10 @@
-#How many distinct renters per month
-
-select
-	left(r.rental_date, 7) as 'Month',
-	count(r.rental_id) as 'Total Rentals',
-	count(distinct r.customer_id) as 'Unique Renters',
-	count(r.rental_id)/count(distinct r.customer_id) as 'Avg Num Rentals Per Renter'
-from
+#Number of distinct renters per month
+SELECT
+	left(r.rental_date, 7) as "Month", count(r.rental_id) as "Total Rentals", 
+	count(distinct r.customer_id) as "Unique Renters", count(r.rental_id)/
+	count(distinct r.customer_id) as "Avg Num Rentals Per Renter"
+FROM
 	rental r
-group by
+GROUP BY
 	1
 ;
