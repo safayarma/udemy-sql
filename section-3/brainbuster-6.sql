319845ed1e214f59bf345b39462539e3c8db5e47
Safayar Mesdaghi-Alazem <safayarma@gmail.com>
2022-02-22 20:12:53 -0500

    - Revisited Topic

--- section-3/brainbuster-6.sql
+++ section-3/brainbuster-6.sql
@@ -1,23 +1,21 @@
-#EVERY CUSTOMER'S LAST RENTAL DATE
-select
-	concat(c.first_name, ' ', c.last_name) as 'Name',
-	c.email as 'Email',
-	max(r.rental_date) as 'Last Rental Date'
-from
-	customer c,
-	rental r	
-where
-	r.customer_id = c.customer_id
-group by
+#Every customers last rental date
+SELECT
+	concat(c.first_name, " ", c.last_name) as "Name", c.email as "Email", MAX(r.rental_date) as "Last Rental Date"
+FROM
+	customer c, rental r
+WHERE
+	c.customer_id = r.customer_id
+GROUP BY
 	c.customer_id
+ORDER BY
+	3
 ;
 
-#REVENUE BY EACH MONTH
-select
-	left(p.payment_date, 7) as 'Month',
-	sum(p.amount) as 'Monthly Revenue'
-from
+#Revenue by each month
+SELECT
+	left(p.payment_date, 7) as "Date", SUM(p.amount) as "Revenue"
+FROM
 	payment p
-group by
+GROUP BY
 	1
 ;
